48a3e36f6b1f845280761e7d39733d0b8de525ae
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-02-11 17:31:26 +0200

    Merge branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/layout/Navbar";
-<<<<<<< HEAD
 import BardingPass from "./pages/flight booking/BardingPass";
 import Checkout from "./pages/flight booking/Checkout";
 import PaymentSuccess from "./pages/flight booking/PaymentSuccess";
@@ -9,14 +8,12 @@ import FirstPage from "./pages/flight booking/FirstPage";
 import ChooseSeat from "./pages/flight booking/ChooseSeat";
 import Flightlayout from "./pages/flight booking/Flightlayout";
 import Flightbooking from "./pages/flight booking/Flightbooking";
-=======
 import HotelBooking from "./hotel_booking/HotelBooking";
 import AvailableRooms from "./hotel_booking/AvailableRooms";
 import HotelAbout from "./hotel_booking/HotelAbout";
 import CheckIn from "./hotel_booking/CheckIn";
 import CheckOut from "./hotel_booking/CheckOut";
 import Done from "./hotel_booking/Done";
->>>>>>> zeyad-hotel_booking
 
 const App = () => {
   return (
@@ -24,7 +21,6 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
         <Route path="/flights" element={<Flightlayout />}>
           <Route index path="" element={<FirstPage />} />
           <Route path="flightbooking" element={<Flightbooking />} />
@@ -33,7 +29,6 @@ const App = () => {
           <Route path="checkout" element={<Checkout />} />
           <Route path="payment-success" element={<PaymentSuccess />} />
         </Route>
-=======
 
         {/* Hotel Booking */}
         <Route path="/hotels" element={<HotelBooking />} />
@@ -42,7 +37,6 @@ const App = () => {
         <Route path="/hotels/checkin" element={<CheckIn />} />
         <Route path="/hotels/checkout" element={<CheckOut />} />
         <Route path="/hotels/checkout/done" element={<Done />} />
->>>>>>> zeyad-hotel_booking
       </Routes>
     </>
   );
