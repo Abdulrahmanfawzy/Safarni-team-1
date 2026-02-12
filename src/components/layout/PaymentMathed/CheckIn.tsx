e0130d764476406745e37735c242e6adf7324063
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-02-12 19:32:40 +0200

    add buton back

--- src/components/layout/PaymentMathed/CheckIn.tsx
+++ src/components/layout/PaymentMathed/CheckIn.tsx
@@ -2,200 +2,210 @@ import { useState } from "react";
 import ImageSection from "../../../pages/hotel_booking/components/ImageSection";
 import { Star } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import Back from "../../common/Back";
 
 const CheckIn = () => {
   const [openSubmit, setOpenSubmit] = useState(false);
   return (
     <>
-      <div className="flex flex-col lg:flex-row justify-around md:m-15 m-0 items-stretch gap-10  mb-24 md:mb-0">
-        {/* Image Section */}
+      <div className="lg:mx-15 lg:my-5 m-0 ">
+        <div className="mb-4  ml-6 hidden md:block">
+          <Back />
+        </div>
+        <div className="flex flex-col lg:flex-row justify-around items-stretch gap-10  mb-24 md:mb-0">
+          {/* Image Section */}
 
-        <ImageSection />
+          <ImageSection />
 
-        {/* Booking Section */}
-        <div className="w-full lg:w-2/5 flex flex-col gap-3 md:px-0 px-3">
-          {/* Hotel Info */}
-          <div className="flex flex-col gap-3 border-b-2 border-b-gray-100 pb-3">
-            <div className="flex justify-between items-center pt-2">
-              <div className="bg-sky-100 py-1.5 px-4 rounded-2xl text-sm font-medium text-sky-900">
-                10% Off
+          {/* Booking Section */}
+          <div className="w-full lg:w-2/5 flex flex-col gap-3 md:px-0 px-3">
+            {/* Hotel Info */}
+            <div className="flex flex-col gap-3 border-b-2 border-b-gray-100 pb-3">
+              <div className="flex justify-between items-center pt-2">
+                <div className="bg-sky-100 py-1.5 px-4 rounded-2xl text-sm font-medium text-sky-900">
+                  10% Off
+                </div>
+
+                <div className="font-semibold flex gap-1 items-center text-gray-500">
+                  <Star className="text-yellow-500 w-5 h-5" />
+                  4.5 (356 reviews)
+                </div>
               </div>
 
-              <div className="font-semibold flex gap-1 items-center text-gray-500">
-                <Star className="text-yellow-500 w-5 h-5" />
-                4.5 (356 reviews)
-              </div>
+              <p className="text-3xl font-medium text-gray-900">
+                HarborHaven Hideaway
+              </p>
+
+              <p className="text-lg font-medium text-gray-500">
+                1012 Ocean Avenue, New York, USA
+              </p>
             </div>
 
-            <p className="text-3xl font-medium text-gray-900">
-              HarborHaven Hideaway
-            </p>
+            {/* Booking Title */}
+            <h2 className="text-center text-2xl font-medium text-blue-700">
+              Book Hotel
+            </h2>
 
-            <p className="text-lg font-medium text-gray-500">
-              1012 Ocean Avenue, New York, USA
-            </p>
-          </div>
+            {/* Check In */}
+            <div className="flex flex-col gap-3">
+              <p className="text-gray-900 text-xl font-medium">Check In</p>
 
-          {/* Booking Title */}
-          <h2 className="text-center text-2xl font-medium text-blue-700">
-            Book Hotel
-          </h2>
-
-          {/* Check In */}
-          <div className="flex flex-col gap-3">
-            <p className="text-gray-900 text-xl font-medium">Check In</p>
-
-            <div
-              className="
+              <div
+                className="
              flex
           gap-4
           overflow-x-auto
           snap-x snap-mandatory
           scrollbar-hide
           md:grid md:grid-cols-4 md:overflow-x-visible"
-            >
-              <div className="bg-sky-100   px-10 py-2 md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer">
-                <p className="font-medium">Today</p>
-                <p className="font-semibold text-lg">4 Oct</p>
-              </div>
-              <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
-                <p>Tomorrow</p>
-                <p>5 Oct</p>
-              </div>
-              <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
-                <p>Wed</p>
-                <p>6 Oct</p>
-              </div>
-              <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
-                <p>Thu</p>
-                <p>7 Oct</p>
+              >
+                <div className="bg-sky-100   px-10 py-2 md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer">
+                  <p className="font-medium">Today</p>
+                  <p className="font-semibold text-lg">4 Oct</p>
+                </div>
+                <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
+                  <p>Tomorrow</p>
+                  <p>5 Oct</p>
+                </div>
+                <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
+                  <p>Wed</p>
+                  <p>6 Oct</p>
+                </div>
+                <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
+                  <p>Thu</p>
+                  <p>7 Oct</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Check Out */}
-          <div className="flex flex-col gap-3">
-            <p className="text-gray-900 text-xl font-medium">Check Out</p>
-            <div
-              className="
+            {/* Check Out */}
+            <div className="flex flex-col gap-3">
+              <p className="text-gray-900 text-xl font-medium">Check Out</p>
+              <div
+                className="
             flex
           gap-4
           overflow-x-auto
           snap-x snap-mandatory
           scrollbar-hide
           md:grid md:grid-cols-4 md:overflow-x-visible"
-            >
-              <div className="bg-sky-100  px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer">
-                <p className="font-medium">Tomorrow</p>
-                <p className="font-semibold text-lg">5 Oct</p>
-              </div>
-              <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
-                <p>Wed</p>
-                <p>6 Oct</p>
-              </div>
-              <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
-                <p>Thu</p>
-                <p>7 Oct</p>
-              </div>
-              <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
-                <p>Fri</p>
-                <p>8 Oct</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-3">
-            <p className="text-gray-900 text-xl font-medium">Note To Owner</p>
-            <textarea
-              name=""
-              id=""
-              className="w-full h-44 bg-gray-100 p-4 text-2xl"
-              placeholder="Enter here"
-            ></textarea>
-          </div>
-
-          <button
-            onClick={() => setOpenSubmit(true)}
-            className="w-full py-3 rounded-md bg-[#1E429F] text-white text-xl font-semibold hover:bg-[#16357a] transition"
-          >
-            Submit
-          </button>
-        </div>
-        {openSubmit && (
-          <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-            <div className="bg-white w-[95%] max-w-xl rounded-2xl shadow-2xl p-10 relative animate-scaleIn">
-              <div className="flex justify-between items-center border-b border-gray-200 pb-4 mb-8">
-                <div>
-                  <p className="text-2xl font-medium text-gray-900">Adults</p>
-                  <p className="text-xl text-gray-500">Ages 18 or above</p>
-                </div>
-
-                <div className="flex items-center gap-3">
-                  <button className="w-9 h-9 flex items-center justify-center rounded-md bg-gray-100 text-xl font-bold hover:bg-gray-200 transition">
-                    -
-                  </button>
-
-                  <span className="w-8 text-center text-lg font-semibold">
-                    1
-                  </span>
-
-                  <button className="w-9 h-9 flex items-center justify-center rounded-md bg-blue-900 text-white text-xl font-bold hover:bg-blue-700 transition">
-                    +
-                  </button>
-                </div>
-              </div>
-
-              <div className="flex justify-between items-center border-b border-gray-200 pb-4 mb-8">
-                <div>
-                  <p className="text-2xl font-medium text-gray-900">Children</p>
-                  <p className="text-xl text-gray-500">Ages 2-17</p>
-                </div>
-
-                <div className="flex items-center gap-3">
-                  <button className="w-9 h-9 flex items-center justify-center rounded-md bg-gray-100 text-xl font-bold hover:bg-gray-200 transition">
-                    -
-                  </button>
-
-                  <span className="w-8 text-center text-lg font-semibold">
-                    1
-                  </span>
-
-                  <button className="w-9 h-9 flex items-center justify-center rounded-md bg-blue-900 text-white text-xl font-bold hover:bg-blue-700 transition">
-                    +
-                  </button>
-                </div>
-              </div>
-
-              <div className="flex justify-between items-center border-b border-gray-200 pb-4 mb-8">
-                <div>
-                  <p className="text-2xl font-medium text-gray-900">Infants</p>
-                  <p className="text-xl text-gray-500">Under Ages 2</p>
-                </div>
-
-                <div className="flex items-center gap-3">
-                  <button className="w-9 h-9 flex items-center justify-center rounded-md bg-gray-100 text-xl font-bold hover:bg-gray-200 transition">
-                    -
-                  </button>
-
-                  <span className="w-8 text-center text-lg font-semibold">
-                    1
-                  </span>
-
-                  <button className="w-9 h-9 flex items-center justify-center rounded-md bg-blue-900 text-white text-xl font-bold hover:bg-blue-700 transition">
-                    +
-                  </button>
-                </div>
-              </div>
-
-              <NavLink
-                to="/checkout"
-                className="block w-full py-3 text-center  rounded-md bg-[#1E429F] text-white text-lg font-semibold hover:bg-[#16357a] transition"
               >
-                Book Now
-              </NavLink>
+                <div className="bg-sky-100  px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer">
+                  <p className="font-medium">Tomorrow</p>
+                  <p className="font-semibold text-lg">5 Oct</p>
+                </div>
+                <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
+                  <p>Wed</p>
+                  <p>6 Oct</p>
+                </div>
+                <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
+                  <p>Thu</p>
+                  <p>7 Oct</p>
+                </div>
+                <div className=" px-10 py-2  md:py-4 md:px-6 rounded-lg text-center border-2 border-gray-100 shadow-xs cursor-pointer hover:border-blue-500">
+                  <p>Fri</p>
+                  <p>8 Oct</p>
+                </div>
+              </div>
             </div>
+
+            <div className="flex flex-col gap-3">
+              <p className="text-gray-900 text-xl font-medium">Note To Owner</p>
+              <textarea
+                name=""
+                id=""
+                className="w-full h-44 bg-gray-100 p-4 text-2xl"
+                placeholder="Enter here"
+              ></textarea>
+            </div>
+
+            <button
+              onClick={() => setOpenSubmit(true)}
+              className="w-full py-3 rounded-md bg-[#1E429F] text-white text-xl font-semibold hover:bg-[#16357a] transition"
+            >
+              Submit
+            </button>
           </div>
-        )}
+          {openSubmit && (
+            <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+              <div className="bg-white w-[95%] max-w-xl rounded-2xl shadow-2xl p-10 relative animate-scaleIn">
+                <div className="flex justify-between items-center border-b border-gray-200 pb-4 mb-8">
+                  <div>
+                    <p className="text-2xl font-medium text-gray-900">Adults</p>
+                    <p className="text-xl text-gray-500">Ages 18 or above</p>
+                  </div>
+
+                  <div className="flex items-center gap-3">
+                    <button className="w-9 h-9 flex items-center justify-center rounded-md bg-gray-100 text-xl font-bold hover:bg-gray-200 transition">
+                      -
+                    </button>
+
+                    <span className="w-8 text-center text-lg font-semibold">
+                      1
+                    </span>
+
+                    <button className="w-9 h-9 flex items-center justify-center rounded-md bg-blue-900 text-white text-xl font-bold hover:bg-blue-700 transition">
+                      +
+                    </button>
+                  </div>
+                </div>
+
+                <div className="flex justify-between items-center border-b border-gray-200 pb-4 mb-8">
+                  <div>
+                    <p className="text-2xl font-medium text-gray-900">
+                      Children
+                    </p>
+                    <p className="text-xl text-gray-500">Ages 2-17</p>
+                  </div>
+
+                  <div className="flex items-center gap-3">
+                    <button className="w-9 h-9 flex items-center justify-center rounded-md bg-gray-100 text-xl font-bold hover:bg-gray-200 transition">
+                      -
+                    </button>
+
+                    <span className="w-8 text-center text-lg font-semibold">
+                      1
+                    </span>
+
+                    <button className="w-9 h-9 flex items-center justify-center rounded-md bg-blue-900 text-white text-xl font-bold hover:bg-blue-700 transition">
+                      +
+                    </button>
+                  </div>
+                </div>
+
+                <div className="flex justify-between items-center border-b border-gray-200 pb-4 mb-8">
+                  <div>
+                    <p className="text-2xl font-medium text-gray-900">
+                      Infants
+                    </p>
+                    <p className="text-xl text-gray-500">Under Ages 2</p>
+                  </div>
+
+                  <div className="flex items-center gap-3">
+                    <button className="w-9 h-9 flex items-center justify-center rounded-md bg-gray-100 text-xl font-bold hover:bg-gray-200 transition">
+                      -
+                    </button>
+
+                    <span className="w-8 text-center text-lg font-semibold">
+                      1
+                    </span>
+
+                    <button className="w-9 h-9 flex items-center justify-center rounded-md bg-blue-900 text-white text-xl font-bold hover:bg-blue-700 transition">
+                      +
+                    </button>
+                  </div>
+                </div>
+
+                <NavLink
+                  to="/checkout"
+                  className="block w-full py-3 text-center  rounded-md bg-[#1E429F] text-white text-lg font-semibold hover:bg-[#16357a] transition"
+                >
+                  Book Now
+                </NavLink>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
--- src/components/layout/PaymentMathed/CheckOut.tsx
+++ src/components/layout/PaymentMathed/CheckOut.tsx
@@ -21,38 +21,22 @@ const CheckOut = () => {
 >>>>>>> e1571c7 (edite response)
 
   return (
-    <div className="flex flex-col lg:flex-row justify-around m-5 lg:m-15 gap-5 items-start  mb-24 md:mb-0">
-      {/* Left Side: Title + Text */}
-      <div className="w-full lg:w-2/5 flex flex-col gap-4  md:hidden">
-        <h2 className="text-left font-medium text-[26px] text-gray-900">
+    <div className="m-5 lg:mx-15 lg:my-5">
+      <div className="mb-4 ml-6 flex items-center gap-10">
+        <Back />
+        <h2 className="font-medium text-[26px] text-gray-900 block md:hidden">
           Payment Method
         </h2>
-        <p className="text-left text-22 text-gray-700 font-medium">
-          Add Your Payment Method
-        </p>
       </div>
 
-      {/* Right Side: Image */}
-      <div className="w-full lg:w-[50%] h-auto bg-gray-100 rounded-md ">
-        <img
-          src={cardImage}
-          alt="cardImage"
-          className="w-full h-full object-cover rounded-2xl"
-        />
-      </div>
-
-      {/* Payment Methods + Form */}
-      <div className="w-full lg:w-2/5 flex flex-col gap-5 ">
-
-        <div className=" hidden  md:flex md:flex-col md:gap-3">
-          <h2 className="text-center font-medium text-[26px] text-gray-900">
-            Payment Method
-          </h2>
+      <div className="flex flex-col lg:flex-row justify-around  gap-5 items-start  mb-24 md:mb-0">
+        <div className="w-full lg:w-2/5 flex flex-col gap-4  md:hidden">
           <p className="text-left text-22 text-gray-700 font-medium">
             Add Your Payment Method
           </p>
         </div>
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         <div className="w-2/5 flex flex-col gap-5">
           <h2 className="text-center font-medium text-[26px] text-gray-900">
@@ -102,25 +86,27 @@ const CheckOut = () => {
             <img src={Visa} alt="" className="w-8" />
             Visa
           </div>
+=======
+        <div className="w-full lg:w-[50%] h-auto bg-gray-100 rounded-md ">
+          <img
+            src={cardImage}
+            alt="cardImage"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+>>>>>>> 0abea69 (add buton back)
         </div>
 
-        {/* Forms */}
-        {methedTab === "paypal" && (
-          <div className="flex flex-col gap-2">
-            <div>
-              <label className="text-lg font-medium text-gray-800">
-                Full Name
-              </label>
-              <div className="flex items-center border-2 px-2 rounded-xs text-gray-400 mb-4">
-                <User2 />
-                <input
-                  type="text"
-                  placeholder="kneeDue@untitledui.com"
-                  className="w-full border-0 p-2.5 text-xl font-normal outline-none focus:outline-none focus:ring-0"
-                />
-              </div>
-            </div>
+        <div className="w-full lg:w-2/5 flex flex-col gap-5 ">
+          <div className=" hidden  md:flex md:flex-col md:gap-3">
+            <h2 className="text-center font-medium text-[26px] text-gray-900">
+              Payment Method
+            </h2>
+            <p className="text-left text-22 text-gray-700 font-medium">
+              Add Your Payment Method
+            </p>
+          </div>
 
+<<<<<<< HEAD
             <div>
               <label className="text-lg font-medium text-gray-800">
                 Your Email
@@ -134,10 +120,33 @@ const CheckOut = () => {
                 />
               </div>
 >>>>>>> e1571c7 (edite response)
+=======
+          <div className="flex justify-evenly flex-wrap gap-2">
+            <div
+              onClick={() => setMethedTab("paypal")}
+              className="bg-gray-100 py-2 px-5 rounded-2xl text-xl font-medium text-gray-600 flex items-center gap-1 cursor-pointer"
+            >
+              <img src={paypal} alt="" className="w-5" />
+              Paypal
+            </div>
+            <div
+              onClick={() => setMethedTab("mastercard")}
+              className="bg-gray-100 py-2 px-5 rounded-2xl text-xl font-medium text-gray-600 flex items-center gap-1 cursor-pointer"
+            >
+              <img src={Mastercard} alt="" className="w-8" />
+              Mastercard
+            </div>
+            <div
+              onClick={() => setMethedTab("visa")}
+              className="bg-gray-100 py-2 px-5 rounded-2xl text-xl font-medium text-gray-600 flex items-center gap-1 cursor-pointer"
+            >
+              <img src={Visa} alt="" className="w-8" />
+              Visa
+>>>>>>> 0abea69 (add buton back)
             </div>
           </div>
-        )}
 
+<<<<<<< HEAD
 <<<<<<< HEAD
           {methedTab === "paypal" && (
             <div className="flex flex-col gap-1">
@@ -182,16 +191,26 @@ const CheckOut = () => {
                 <label className="text-lg font-medium text-gray-800">
                   Valid Date
 >>>>>>> e1571c7 (edite response)
+=======
+          {/* Forms */}
+          {methedTab === "paypal" && (
+            <div className="flex flex-col gap-2">
+              <div>
+                <label className="text-lg font-medium text-gray-800">
+                  Full Name
+>>>>>>> 0abea69 (add buton back)
                 </label>
                 <div className="flex items-center border-2 px-2 rounded-xs text-gray-400 mb-4">
+                  <User2 />
                   <input
                     type="text"
-                    placeholder="12-6-2024"
+                    placeholder="kneeDue@untitledui.com"
                     className="w-full border-0 p-2.5 text-xl font-normal outline-none focus:outline-none focus:ring-0"
                   />
                 </div>
               </div>
 
+<<<<<<< HEAD
 <<<<<<< HEAD
               <div>
                 <label
@@ -203,16 +222,23 @@ const CheckOut = () => {
               <div className="w-[20%]">
                 <label className="text-lg font-medium text-gray-800">Cvv</label>
 >>>>>>> e1571c7 (edite response)
+=======
+              <div>
+                <label className="text-lg font-medium text-gray-800">
+                  Your Email
+                </label>
+>>>>>>> 0abea69 (add buton back)
                 <div className="flex items-center border-2 px-2 rounded-xs text-gray-400 mb-4">
+                  <Mail />
                   <input
                     type="text"
-                    placeholder="522"
+                    placeholder="kneeDue@untitledui.com"
                     className="w-full border-0 p-2.5 text-xl font-normal outline-none focus:outline-none focus:ring-0"
                   />
-                  <CreditCardIcon />
                 </div>
               </div>
             </div>
+<<<<<<< HEAD
 <<<<<<< HEAD
           )}
           {methedTab === "mastercard" && (
@@ -389,19 +415,27 @@ const CheckOut = () => {
 =======
           </div>
         )}
+=======
+          )}
+>>>>>>> 0abea69 (add buton back)
 
-        {methedTab === "" && (
-          <div className="flex justify-center">
-            <button
-              type="button"
-              className="flex items-center gap-2 text-gray-900 px-6 py-3 text-xl font-medium"
-            >
-              Add Card
-              <CirclePlus className="w-6 h-6" />
-            </button>
-          </div>
-        )}
+          {(methedTab === "mastercard" || methedTab === "visa") && (
+            <div className="flex flex-col gap-2">
+              <div>
+                <label className="text-lg font-medium text-gray-800">
+                  Full Name
+                </label>
+                <div className="flex items-center border-2 px-2 rounded-xs text-gray-400 mb-4">
+                  <User2 />
+                  <input
+                    type="text"
+                    placeholder="kneeDue@untitledui.com"
+                    className="w-full border-0 p-2.5 text-xl font-normal outline-none focus:outline-none focus:ring-0"
+                  />
+                </div>
+              </div>
 
+<<<<<<< HEAD
         <NavLink
           to="/checkout/done"
           className="block w-full py-3 text-center rounded-md bg-[#1E429F] text-white text-lg font-semibold hover:bg-[#16357a] transition"
@@ -409,6 +443,72 @@ const CheckOut = () => {
           Continue
         </NavLink>
 >>>>>>> e1571c7 (edite response)
+=======
+              <div>
+                <label className="text-lg font-medium text-gray-800">
+                  Your Email
+                </label>
+                <div className="flex items-center border-2 px-2 rounded-xs text-gray-400 mb-4">
+                  <Mail />
+                  <input
+                    type="text"
+                    placeholder="kneeDue@untitledui.com"
+                    className="w-full border-0 p-2.5 text-xl font-normal outline-none focus:outline-none focus:ring-0"
+                  />
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                <div className="w-[75%]">
+                  <label className="text-lg font-medium text-gray-800">
+                    Valid Date
+                  </label>
+                  <div className="flex items-center border-2 px-2 rounded-xs text-gray-400 mb-4">
+                    <input
+                      type="text"
+                      placeholder="12-6-2024"
+                      className="w-full border-0 p-2.5 text-xl font-normal outline-none focus:outline-none focus:ring-0"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-[20%]">
+                  <label className="text-lg font-medium text-gray-800">
+                    Cvv
+                  </label>
+                  <div className="flex items-center border-2 px-2 rounded-xs text-gray-400 mb-4">
+                    <input
+                      type="text"
+                      placeholder="522"
+                      className="w-full border-0 p-2.5 text-xl font-normal outline-none focus:outline-none focus:ring-0"
+                    />
+                    <CreditCardIcon />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {methedTab === "" && (
+            <div className="flex justify-center">
+              <button
+                type="button"
+                className="flex items-center gap-2 text-gray-900 px-6 py-3 text-xl font-medium"
+              >
+                Add Card
+                <CirclePlus className="w-6 h-6" />
+              </button>
+            </div>
+          )}
+
+          <NavLink
+            to="/checkout/done"
+            className="block w-full py-3 text-center rounded-md bg-[#1E429F] text-white text-lg font-semibold hover:bg-[#16357a] transition"
+          >
+            Continue
+          </NavLink>
+        </div>
+>>>>>>> 0abea69 (add buton back)
       </div>
     </div>
   );
