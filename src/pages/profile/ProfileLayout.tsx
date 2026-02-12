141144fac5a1e393d3d32d01076270848cb943f7
Younes ASSASLA <assaslayounes@yahoo.fr>
2026-02-12 23:39:53 +0100

    add container to profile page

--- src/pages/profile/ProfileLayout.tsx
+++ src/pages/profile/ProfileLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export function ProfileLayout() {
     return (
-        <div className="flex min-h-screen flex-col bg-white">
+        <div className=" container mx-auto flex min-h-screen flex-col bg-white">
             <Outlet />
         </div>
     );
